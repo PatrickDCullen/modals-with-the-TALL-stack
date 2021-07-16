7dc232818c41606eb95588d83548850a7ffbfb87
PatrickDCullen <cullenpatrickd@gmail.com>
2021-07-16 13:32:20 -0400

    End Ep 1: The HTML and CSS for a Basic Modal

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+
+<title>Modal Learning</title>
+<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+@livewireStyles
+
+<body class="bg-gray-200">
+    {{ $slot }}
+
+    @livewireScripts
+</body>
