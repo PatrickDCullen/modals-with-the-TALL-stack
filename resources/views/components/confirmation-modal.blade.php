29edd63b5da58163a2813f615d0710f6895cc0c6
PatrickDCullen <cullenpatrickd@gmail.com>
2021-07-17 13:44:13 -0400

    End Ep 6: Livewire and Alpine Synchronization

--- resources/views/components/confirmation-modal.blade.php
+++ resources/views/components/confirmation-modal.blade.php
@@ -1,5 +1,6 @@
-<div>
-    <div class="fixed inset-0 bg-gray-900 opacity-90"></div>
+<div {{ $attributes }} x-data="{ show: @entangle($attributes->wire('model')) }" x-show="show" @keydown.escape.window="show = false">
+    <div class="fixed inset-0 bg-gray-900 opacity-90" @click="show = false"></div>
+
     <div class="bg-white shadow-md p-4 max-w-sm h-48 m-auto rounded-md fixed inset-0">
         <div class="flex flex-col h-full justify-between">
             <header>
